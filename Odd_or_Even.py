9c98f5fcf319395c3c2c0770cc0fdff4ae6794f1
Johns Jose <jjkanichukattu@gmail.com>
2024-10-26 20:42:53 +0530

    Add files via upload

---
+++ Odd_or_Even.py
@@ -0,0 +1,9 @@
+'''Authore: Johns Jose
+Date: 19-10-2024
+Description: write a program to check whether the number is even or odd'''
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("The given Number ",number," is even.")
+else:
+    print("The given Number ",number," is odd.")
